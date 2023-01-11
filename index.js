7b02db11295a64baf99c290c55c8113e56bc3d37
Code Sandbox <hello@codesandbox.io>
2023-01-11 01:56:42 +0000

    Deployment from Codesandbox.

---
+++ index.js
@@ -0,0 +1,13 @@
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
+
+// tell Barba to use the css plugin
+barba.use(barbaCss);
+
+// init Barba
+barba.init({
+    transitions: [{
+        once(){}
+        // create your amazing enter animation here
+    }]
+ });
